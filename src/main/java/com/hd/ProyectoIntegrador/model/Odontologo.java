c5afdc69762830fcaa7a8ac29f1326d3d232ab6c
Bruno Gallegos <brunoooogallegos@hotmail.com>
2024-09-05 20:06:22 -0500

    feat added turno reference in Odontologo

--- src/main/java/com/hd/ProyectoIntegrador/model/Odontologo.java
+++ src/main/java/com/hd/ProyectoIntegrador/model/Odontologo.java
@@ -3,6 +3,9 @@ package com.hd.ProyectoIntegrador.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Entity
 @Table(name = "odontologos")
@@ -18,4 +21,6 @@ public class Odontologo {
     private String matricula;
     private String nombre;
     private String apellido;
+    @OneToMany(mappedBy = "odontologo")
+    private Set<Turno> turnos = new HashSet<>();
 }
